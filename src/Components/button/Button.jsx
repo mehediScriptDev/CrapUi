190ed90912fe45160edb3426e0fa0c9719aabfe8
mehediScriptDev <mehedi25122003@gmail.com>
2025-12-02 21:57:35 +0600

    new style added

--- src/Components/button/Button.jsx
+++ src/Components/button/Button.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-const Button = () => {
-  return <div></div>;
+const Button = ({ text, bgColor, Color }) => {
+  return (
+    <div>
+      <button className={`${bgColor} ${Color} px-5 py-1.5`}>{text}</button>
+    </div>
+  );
 };
 
 export default Button;
