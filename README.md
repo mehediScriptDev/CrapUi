b7e8b8f39e4f475f8fec7e00988cb80834138701
Mehedi <mehedi25122003@gmail.com>
2026-01-11 20:29:14 +0600

    template added

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # CrapUi
-A small collection of React + Tailwind components I build daily...simple and reusable...
+A small collection of React + Tailwind components I build daily...simple and reusable.
+<img src="crapui.jpg"/>
--- templates/basic/src/main.jsx
+++ templates/basic/src/main.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
 import './index.css'
+import { RouterProvider } from "react-router/dom";
+import { router } from './router/routes.jsx';
+// import { AuthProvider } from './components/AuthProvider.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <StrictMode>
+   
+      {/* <AuthProvider> */}
+        <RouterProvider router={router} />
+        
+      {/* </AuthProvider> */}
+    
+  </StrictMode>,
 )
